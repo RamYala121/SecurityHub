c3c63aab8cce7b2b55722d9aba835cff87731f53
SoggyTM <thetaseen1@gmail.com>
2025-06-24 20:35:55 -0400

    Update WebsiteBlocker.py

--- WebsiteBlocker.py
+++ WebsiteBlocker.py
@@ -1,3 +1,10 @@
+website = input("Type in the website you wish to block: ")
+
 host = r"C:\Windows\System32\drivers\etc\hosts"
 
-f = open(host,"rt")
+with open(host,"rt") as f:
+    for x in f:
+        if x.find(website)>=0:
+            print("This website has already been blocked. Please try again.")
+        else:
+   
